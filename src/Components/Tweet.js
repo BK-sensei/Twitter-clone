be0fd8c6bc866c8673347a6b03e7925061ef3245
unknown <jochong27@gmail.com>
2022-01-21 23:25:47 +0100

    retweeter

--- src/Components/Tweet.js
+++ src/Components/Tweet.js
@@ -1,5 +1,10 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
 import { Link } from 'react-router-dom'
+
+import moment from "moment"
+import 'moment/locale/fr'
+
+import { UserContext } from '../Context/UserContext'
 
 import styled from 'styled-components'
 
@@ -37,19 +42,28 @@ const FooterZone = styled.a`
 `
 
 const Tweet = (props) => {
-  const { id, name, username, userid, createdAt, text, numRetweets, numComments } = props
+  moment.locale("fr")
+  const { user } = useContext(UserContext)
+  const { id, name, username, userid, createdAt, text, numRetweets, numComments, retweets } = props
+  const [nbRetweets, setNbRetweets] = useState(numRetweets)
 
-  const handleRetweet = (id) => {
-    // async () =>{
+  const handleRetweet = () => {
+    const newArrayRetweet = [...retweets, user._id]
+    retweeter(newArrayRetweet)
+    setNbRetweets(nbRetweets + 1)
+  }
 
-    //   await fetch(`http://localhost:5000/tweets/${id}`, {
-    //     method:'put',
-    //     credentials: "include",
-    //     body: JSON.stringify(user._id)
-        
-    //   })
-
-    // }
+  const retweeter = async (newArrayRetweet) => {
+    await fetch(`http://localhost:5000/tweets/${id}`, {
+      method:'put',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        user: newArrayRetweet
+      })
+    })
   }
 
   // console.log(createdAt)
@@ -68,7 +82,7 @@ const Tweet = (props) => {
           <Link to={`/user/${userid}`}>
             <h6>{name}</h6> 
           </Link>
-          @{username} · {createdAt}
+          @{username} · {moment(createdAt).format('lll')}
         </Header>
         <Middle>
           {text}
@@ -82,12 +96,12 @@ const Tweet = (props) => {
               {numComments}
             </FooterNum>
           </FooterZone>
-          <FooterZone onClick={() => handleRetweet(id)}>
+          <FooterZone onClick={() => handleRetweet()}>
             <FooterIcon>
               <svg viewBox="0 0 24 24" aria-hidden="true" className=""><g><path d="M23.77 15.67c-.292-.293-.767-.293-1.06 0l-2.22 2.22V7.65c0-2.068-1.683-3.75-3.75-3.75h-5.85c-.414 0-.75.336-.75.75s.336.75.75.75h5.85c1.24 0 2.25 1.01 2.25 2.25v10.24l-2.22-2.22c-.293-.293-.768-.293-1.06 0s-.294.768 0 1.06l3.5 3.5c.145.147.337.22.53.22s.383-.072.53-.22l3.5-3.5c.294-.292.294-.767 0-1.06zm-10.66 3.28H7.26c-1.24 0-2.25-1.01-2.25-2.25V6.46l2.22 2.22c.148.147.34.22.532.22s.384-.073.53-.22c.293-.293.293-.768 0-1.06l-3.5-3.5c-.293-.294-.768-.294-1.06 0l-3.5 3.5c-.294.292-.294.767 0 1.06s.767.293 1.06 0l2.22-2.22V16.7c0 2.068 1.683 3.75 3.75 3.75h5.85c.414 0 .75-.336.75-.75s-.337-.75-.75-.75z"></path></g></svg>
             </FooterIcon>
             <FooterNum>
-              {numRetweets}
+              {nbRetweets}
             </FooterNum>
           </FooterZone>
         </Footer>
